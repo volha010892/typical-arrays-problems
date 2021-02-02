db3aaa45c8eaa68a1c4bbf0c291e92d12f6bfe1b
volha010892 <66121147+volha010892@users.noreply.github.com>
2021-02-02 15:05:37 +0300

    add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if(!array||array.length===0)
   return 0;
+  else
+  return Math.min.apply(null,array);
 }
 
 exports.max = function max (array) {
+  if(!array||array.length===0)
   return 0;
+  else
+  return Math.max.apply(null,array);
 }
 
 exports.avg = function avg (array) {
+  if(!array||array.length===0)
   return 0;
+  else
+  return (array.reduce((sum, el)=>sum+el, 0))/array.length;
 }
